7ddb1002bcc23739da682cf7fa2b222d97e4fa93
Jeremiah <44993244+jechton@users.noreply.github.com>
2026-08-01 21:32:14 -0400

    persist whole vscode folders

--- modules/home/dev/vscode.nix
+++ modules/home/dev/vscode.nix
@@ -5,15 +5,8 @@
     package = pkgs.vscode-fhs;
   };
 
-  home.persistence."/persist" = {
-    directories = [
-      ".vscode"
-      ".config/Code/User/extensions"
-      ".config/Code/User/globalStorage"
-    ];
-    files = [
-      ".config/Code/User/settings.json"
-      ".config/Code/User/keybindings.json"
-    ];
-  };
+  home.persistence."/persist".directories = [
+    ".vscode"
+    ".config/Code"
+  ];
 }

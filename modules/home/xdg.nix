84037bc826ff5286d07344619b1c5a8624e1fe38
Jeremiah <44993244+jechton@users.noreply.github.com>
2026-08-30 03:17:19 -0400

    remove unused pythonrc

--- modules/home/xdg.nix
+++ modules/home/xdg.nix
@@ -9,44 +9,10 @@ let
 in
 {
   xdg.configFile = {
-    "npm/npmrc".text = ''
+    "npm/npmrc".text = /* ini */ ''
       prefix=${cfg.dataHome}/npm
       cache=${cfg.cacheHome}/npm
       init-module=${cfg.configHome}/npm/config/npm-init.js
-    '';
-
-    "python/pythonrc".text = ''
-      import os
-      import atexit
-      import readline
-      from pathlib import Path
-
-      if readline.get_current_history_length() == 0:
-
-          state_home = os.environ.get("XDG_STATE_HOME")
-          if state_home is None:
-              state_home = Path.home() / ".local" / "state"
-          else:
-              state_home = Path(state_home)
-
-          history_path = state_home / "python_history"
-          if history_path.is_dir():
-              raise OSError(f"'{history_path}' cannot be a directory")
-
-          history = str(history_path)
-
-          try:
-              readline.read_history_file(history)
-          except OSError: # Non existent
-              pass
-
-          def write_history():
-              try:
-                  readline.write_history_file(history)
-              except OSError:
-                  pass
-
-          atexit.register(write_history)
     '';
   };
 
